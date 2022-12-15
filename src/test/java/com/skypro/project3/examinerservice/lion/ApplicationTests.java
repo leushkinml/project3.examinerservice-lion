8a27f85c99b3ef7a17827673e38f66425a3d5e46
leush <leushkinml@mail.ru>
2022-12-15 21:29:49 +0300

    Initial commit

---
+++ src/test/java/com/skypro/project3/examinerservice/lion/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.project3.examinerservice.lion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
