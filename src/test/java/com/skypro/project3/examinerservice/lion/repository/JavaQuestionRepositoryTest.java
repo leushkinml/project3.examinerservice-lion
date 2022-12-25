9d3d8c0df68e0e316c28b6f59818fc01008dc57e
leush <leushkinml@mail.ru>
2022-12-25 20:44:01 +0300

    25.12.2022  20:41 Без Mastermind
    23.12.2022 00:16
    
    21.12.2022   01:42
    Написан сервис
    
    16.12.2022   23:57

---
+++ src/test/java/com/skypro/project3/examinerservice/lion/repository/JavaQuestionRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.skypro.project3.examinerservice.lion.repository;
+
+import com.skypro.project3.examinerservice.lion.exception.BadRequestException;
+import com.skypro.project3.examinerservice.lion.exception.NotFoundQuestionException;
+import com.skypro.project3.examinerservice.lion.model.Question;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaQuestionRepositoryTest {
+
+    private JavaQuestionRepository out;
+
+    @BeforeEach
+    void setUp() {
+        out = new JavaQuestionRepository();
+        out.add(new Question("JavaQuestion1", "JavaAnswer1"));
+        out.add(new Question("JavaQuestion2", "JavaAnswer2"));
+        out.add(new Question("JavaQuestion3", "JavaAnswer3"));
+    }
+
+    @Test
+    void addNewQuestion() {
+        Question expected = new Question("JavaQuestion4", "JavaAnswer4");
+        Question actual = out.add(expected);
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertTrue(out.getAll().contains(expected));
+        Assertions.assertEquals(out.getAll().size(), 4);
+    }
+
+    @Test
+    void addExistingQuestion() {
+        Question expected = new Question("JavaQuestion3", "JavaAnswer3");
+        Assertions.assertTrue(out.getAll().contains(expected));
+        Question actual = out.add(expected);
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(out.getAll().size(), 3);
+    }
+
+
+//    @Test
+//    void addNullQuestion() {
+//        Assertions.assertThatExceptionOfType(BadRequestException.class).isThrownBy(() -> {
+//            out.add(null);
+//        });
+//    }
+
+    @Test
+    void removeExistingQuestion() {
+        Question expected = new Question("JavaQuestion1", "JavaAnswer1");
+        Question actual = out.remove(expected);
+        Assertions.assertEquals(expected, actual);
+        System.out.println(out.getAll());
+        Assertions.assertEquals(out.getAll().size(), 2);
+
+    }
+
+//    @Test
+//    void removeNotExistingQuestion() {
+//        Assertions.assertThatExceptionOfType(NotFoundQuestionException.class).isThrownBy(() -> {
+//            out.remove(null);
+//        });
+//    }
+
+    @Test
+    void getAll() {
+        Assertions.assertEquals(out.getAll().size(), 3);
+    }
+}
---
+++ src/test/java/com/skypro/project3/examinerservice/lion/repository/MathQuestionRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.skypro.project3.examinerservice.lion.repository;
+
+import com.skypro.project3.examinerservice.lion.model.Question;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathQuestionRepositoryTest {
+
+    private MathQuestionRepository out;
+
+    @BeforeEach
+    void setUp() {
+        out = new MathQuestionRepository();
+        out.add(new Question("MathQuestion1", "MathAnswer1"));
+        out.add(new Question("MathQuestion2", "MathAnswer2"));
+        out.add(new Question("MathQuestion3", "MathAnswer3"));
+    }
+
+    @Test
+    void addNewQuestion() {
+        Question expected = new Question("MathQuestion4", "MathAnswer4");
+        Question actual = out.add(expected);
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertTrue(out.getAll().contains(expected));
+        Assertions.assertEquals(out.getAll().size(), 4);
+    }
+
+    @Test
+    void addExistingQuestion() {
+        Question expected = new Question("MathQuestion3", "MathAnswer3");
+        Assertions.assertTrue(out.getAll().contains(expected));
+        Question actual = out.add(expected);
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(out.getAll().size(), 3);
+    }
+
+
+//    @Test
+//    void addNullQuestion() {
+//        Assertions.assertThatExceptionOfType(BadRequestException.class).isThrownBy(() -> {
+//            out.add(null);
+//        });
+//    }
+
+    @Test
+    void removeExistingQuestion() {
+        Question expected = new Question("MathQuestion1", "MathAnswer1");
+        Question actual = out.remove(expected);
+        Assertions.assertEquals(expected, actual);
+        System.out.println(out.getAll());
+        Assertions.assertEquals(out.getAll().size(), 2);
+
+    }
+
+//    @Test
+//    void removeNotExistingQuestion() {
+//        Assertions.assertThatExceptionOfType(NotFoundQuestionException.class).isThrownBy(() -> {
+//            out.remove(null);
+//        });
+//    }
+
+    @Test
+    void getAll() {
+        Assertions.assertEquals(out.getAll().size(), 3);
+    }
+}
